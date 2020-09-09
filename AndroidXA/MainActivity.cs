b2e655eac09705e5f42e87fb9d4866cc8ea008d1
Abraham Qian <qianyonglian@outlook.com>
2020-09-09 17:38:56 +0800

    tweak2

--- AndroidXA/MainActivity.cs
+++ AndroidXA/MainActivity.cs
@@ -33,9 +33,10 @@ namespace AndroidXA
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
+            
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
         }
     }
 }
