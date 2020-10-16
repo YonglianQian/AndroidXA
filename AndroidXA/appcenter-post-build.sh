1701829d17bc3ad34bc7a03a7262ee3d93544b15
Abraham Qian <qianyonglian@outlook.com>
2020-10-16 17:17:37 +0800

    modify post-build script2

--- AndroidXA/appcenter-post-build.sh
+++ AndroidXA/appcenter-post-build.sh
@@ -2,4 +2,7 @@ echo "abcd"
 echo $key1
 echo $key2
 echo $key1+$key2
+ls $APPCENTER_OUTPUT_DIRECTORY
+echo "................"
+##/Users/runner/Library/Android/sdk/build-tools/29.0.2/apksigner.bat verify --verbose --print-certs ##$APPCENTER_OUTPUT_DIRECTORY/
 echo "-----------------"
