b08fd9e0dd7a94acd66b10c71d3b5d673f43fd36
Abraham Qian <qianyonglian@outlook.com>
2020-10-16 17:49:51 +0800

    for honour

--- AndroidXA/appcenter-post-build.sh
+++ AndroidXA/appcenter-post-build.sh
@@ -4,8 +4,8 @@ echo $key2
 echo $key1+$key2
 echo $APPCENTER_OUTPUT_DIRECTORY
 echo "............"
-ls $APPCENTER_OUTPUT_DIRECTORY
+ls /Users/runner/Library/Android/sdk/build-tools/29.0.2/
 echo ".........."
 echo "................美丽的泡沫"
-/Users/runner/Library/Android/sdk/build-tools/29.0.2/apksigner.bat verify --verbose --print-certs $APPCENTER_OUTPUT_DIRECTORY
+#/Users/runner/Library/Android/sdk/build-tools/29.0.2/apksigner.bat #verify --verbose --print-certs #$APPCENTER_OUTPUT_DIRECTORY/com.companyname.androidxa.apk
 echo "-----------------"
