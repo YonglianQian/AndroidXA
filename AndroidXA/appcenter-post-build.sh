3bbe232d73a6147873af8005f7cb4f010ef06225
Abraham Qian <qianyonglian@outlook.com>
2020-10-16 17:39:42 +0800

    tweak1

--- AndroidXA/appcenter-post-build.sh
+++ AndroidXA/appcenter-post-build.sh
@@ -2,7 +2,10 @@ echo "abcd"
 echo $key1
 echo $key2
 echo $key1+$key2
-
+echo $APPCENTER_OUTPUT_DIRECTORY
+echo "............"
+ls $APPCENTER_OUTPUT_DIRECTORY
+echo ".........."
 echo "................美丽的泡沫"
 /Users/runner/Library/Android/sdk/build-tools/29.0.2/apksigner.bat verify --verbose --print-certs $APPCENTER_OUTPUT_DIRECTORY
 echo "-----------------"
