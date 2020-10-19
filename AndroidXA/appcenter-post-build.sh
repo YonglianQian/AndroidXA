403784611cf4e75f96228bf8cf2e6d8c2d6062d9
Abraham Qian <qianyonglian@outlook.com>
2020-10-19 11:41:49 +0800

    tweak1

--- AndroidXA/appcenter-post-build.sh
+++ AndroidXA/appcenter-post-build.sh
@@ -9,8 +9,9 @@ echo "................"
 /Users/runner/Library/Android/sdk/build-tools/29.0.2/apksigner verify --verbose --print-certs $APPCENTER_OUTPUT_DIRECTORY/com.companyname.androidxa.apk
 echo "------Start signing-----------"
 cd $APPCENTER_OUTPUT_DIRECTORY
+ls
 echo "1.-------zipalign-------"
-/Users/runner/Library/Android/sdk/build-tools/29.0.2/zipzlign -v -p 4 com.companyname.androidxa.apk my-app-unsigned-aligned.apk
+/Users/runner/Library/Android/sdk/build-tools/29.0.2/zipalign -v -p 4 com.companyname.androidxa.apk my-app-unsigned-aligned.apk
 echo "2. remove original apk........"
 rm com.companyname.androidxa.apk
 echo "3.-------signing........."
