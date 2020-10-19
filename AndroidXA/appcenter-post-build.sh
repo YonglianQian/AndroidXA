571b6cfeab86a49edea2507c4297fc727674f4a8
Abraham Qian <qianyonglian@outlook.com>
2020-10-19 14:44:46 +0800

    tweak4

--- AndroidXA/appcenter-post-build.sh
+++ AndroidXA/appcenter-post-build.sh
@@ -1,24 +1,23 @@
-echo "abcd"
-echo $key1
-echo $key2
-echo $key1+$key2
-echo $APPCENTER_OUTPUT_DIRECTORY
-echo "list current directory............"
-ls
-echo "................"
+echo ".......verify the Release build Apk file........."
 /Users/runner/Library/Android/sdk/build-tools/29.0.2/apksigner verify --verbose --print-certs $APPCENTER_OUTPUT_DIRECTORY/com.companyname.androidxa.apk
 echo "------Start signing-----------"
+
+ls $APPCENTER_SOURCE_DIRECTORY
+
 cd $APPCENTER_OUTPUT_DIRECTORY
 ls
 echo "1.-------zipalign-------"
 /Users/runner/Library/Android/sdk/build-tools/29.0.2/zipalign -v -p 4 com.companyname.androidxa.apk my-app-unsigned-aligned.apk
-echo "2. remove original apk........"
+
+echo "2.-------remove original apk........"
 rm com.companyname.androidxa.apk
+
 echo "3.-------signing........."
 /Users/runner/Library/Android/sdk/build-tools/29.0.2/apksigner sign --ks /Users/runner/work/1/s/AndroidXA/my-release-key.jks --ks-pass pass:"123456" --out com.companyname.androidxa.apk my-app-unsigned-aligned.apk
-echo "4.5 -----ls"
+
+echo "4.-----remove cached file---"
 rm my-app-unsigned-aligned.apk
-ls
-echo "4. completed, verify it"
+
+echo "5.----completed, verify it----"
 /Users/runner/Library/Android/sdk/build-tools/29.0.2/apksigner verify --verbose --print-certs com.companyname.androidxa.apk
 
