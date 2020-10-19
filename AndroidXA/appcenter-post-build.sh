0873d3a3a5db1df72b316efc76f37a21636531a9
Abraham Qian <qianyonglian@outlook.com>
2020-10-19 17:45:13 +0800

    tweak7

--- AndroidXA/appcenter-post-build.sh
+++ AndroidXA/appcenter-post-build.sh
@@ -1,6 +1,3 @@
-echo "..........."
-ls $APPCENTER_SOURCE_DIRECTORY
-
 echo ".......verify the Release build Apk file........."
 $ANDROID_HOME/build-tools/29.0.2/apksigner verify --verbose --print-certs $APPCENTER_OUTPUT_DIRECTORY/com.companyname.androidxa.apk
 echo "------Start signing-----------"
@@ -10,7 +7,7 @@ $ANDROID_HOME/build-tools/29.0.2/zipalign -v -p 4 com.companyname.androidxa.apk 
 echo "2.-------remove original apk------"
 rm com.companyname.androidxa.apk
 echo "3.-------signing-------"
-$ANDROID_HOME/build-tools/29.0.2/apksigner sign --ks $APPCENTER_SOURCE_DIRECTORY/my-release-key.jks --ks-pass pass:"123456" --out com.companyname.androidxa.apk my-app-unsigned-aligned.apk
+$ANDROID_HOME/build-tools/29.0.2/apksigner sign --ks $APPCENTER_SOURCE_DIRECTORY/AndroidXA/my-release-key.jks --ks-pass pass:"123456" --out com.companyname.androidxa.apk my-app-unsigned-aligned.apk
 echo "4.-----remove cached file------"
 rm my-app-unsigned-aligned.apk
 echo "5.----completed, verify it----"
