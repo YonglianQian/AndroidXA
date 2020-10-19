5ff2cf2ba866544ce76ec05cd008b353caf6846b
Abraham Qian <qianyonglian@outlook.com>
2020-10-19 17:29:23 +0800

    tweak5

--- AndroidXA/appcenter-post-build.sh
+++ AndroidXA/appcenter-post-build.sh
@@ -1,20 +1,15 @@
-echo "....find variables to match /Users/runner/library/Android/sdk"
-echo $ANDROID_HOME
-ls $ANDROID_HOME
-ls $ANDROID_HOME/build-tools/
-
 echo ".......verify the Release build Apk file........."
-/Users/runner/Library/Android/sdk/build-tools/29.0.2/apksigner verify --verbose --print-certs $APPCENTER_OUTPUT_DIRECTORY/com.companyname.androidxa.apk
+$ANDROID_HOME/build-tools/29.0.2/apksigner verify --verbose --print-certs $APPCENTER_OUTPUT_DIRECTORY/com.companyname.androidxa.apk
 echo "------Start signing-----------"
 cd $APPCENTER_OUTPUT_DIRECTORY
 echo "1.-------zipalign-------"
-/Users/runner/Library/Android/sdk/build-tools/29.0.2/zipalign -v -p 4 com.companyname.androidxa.apk my-app-unsigned-aligned.apk
+$ANDROID_HOME/build-tools/29.0.2/zipalign -v -p 4 com.companyname.androidxa.apk my-app-unsigned-aligned.apk
 echo "2.-------remove original apk------"
 rm com.companyname.androidxa.apk
 echo "3.-------signing-------"
-/Users/runner/Library/Android/sdk/build-tools/29.0.2/apksigner sign --ks /Users/runner/work/1/s/AndroidXA/my-release-key.jks --ks-pass pass:"123456" --out com.companyname.androidxa.apk my-app-unsigned-aligned.apk
+$ANDROID_HOME/build-tools/29.0.2/apksigner sign --ks $APPCENTER_SOURCE_DIRECTORY/my-release-key.jks --ks-pass pass:"123456" --out com.companyname.androidxa.apk my-app-unsigned-aligned.apk
 echo "4.-----remove cached file------"
 rm my-app-unsigned-aligned.apk
 echo "5.----completed, verify it----"
-/Users/runner/Library/Android/sdk/build-tools/29.0.2/apksigner verify --verbose --print-certs com.companyname.androidxa.apk
+$ANDROID_HOME/build-tools/29.0.2/apksigner verify --verbose --print-certs com.companyname.androidxa.apk
 
