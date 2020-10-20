62f8ab859dcd0a0218ebbbe8f64154b4092124db
Abraham Qian <qianyonglian@outlook.com>
2020-10-20 16:44:16 +0800

    tweak1

--- AndroidXA/appcenter-pre-build.sh
+++ AndroidXA/appcenter-pre-build.sh
@@ -1,13 +1,13 @@
-echo "-----test1---"
-##echo "zhushi"
-echo "-----key1----"
-echo $key1
-echo "----key2-----"
-echo $key2
-echo "........."
-echo $APPCENTER_KEYSTORE_PASSWORD
-echo $APPCENTER_KEY_ALIAS
-echo $APPCENTER_KEY_PASSWORD
+echo "-----Android SDK---"
+ls $Android_Home/build-tools/
+echo "-----node----"
+node --version
+npm --version
+echo "----JDK-----"
+java -version
+
+
+
 
 
 
