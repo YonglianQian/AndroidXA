94ca1a29f37f2cfb6417a2ed863c75480285aac6
Abraham Qian <qianyonglian@outlook.com>
2020-10-21 09:42:01 +0800

    enable double signing schemes

--- AndroidXA/appcenter-post-build.sh
+++ AndroidXA/appcenter-post-build.sh
@@ -1,15 +1,15 @@
-#echo ".......verify the Release build Apk file........."
-#$ANDROID_HOME/build-tools/29.0.2/apksigner verify --verbose --print-certs $APPCENTER_OUTPUT_DIRECTORY/com.companyname.androidxa.apk
-#echo "------Start signing-----------"
-#cd $APPCENTER_OUTPUT_DIRECTORY
-#echo "1.-------zipalign-------"
-#$ANDROID_HOME/build-tools/29.0.2/zipalign -v -p 4 com.companyname.androidxa.apk my-app-unsigned-aligned.apk
-#echo "2.-------remove original apk------"
-#rm com.companyname.androidxa.apk
-#echo "3.-------signing-------"
-#$ANDROID_HOME/build-tools/29.0.2/apksigner sign --ks $APPCENTER_SOURCE_DIRECTORY/AndroidXA/my-release-key.jks --ks-pass pass:"123456" --out com.companyname.androidxa.apk my-app-unsigned-#aligned.apk
-#echo "4.-----remove cached file------"
-#rm my-app-unsigned-aligned.apk
-#echo "5.----completed, verify it----"
-#$ANDROID_HOME/build-tools/29.0.2/apksigner verify --verbose --print-certs com.companyname.androidxa.apk
+echo ".......verify the Release build Apk file........."
+$ANDROID_HOME/build-tools/29.0.2/apksigner verify --verbose --print-certs $APPCENTER_OUTPUT_DIRECTORY/com.companyname.androidxa.apk
+echo "------Start signing-----------"
+cd $APPCENTER_OUTPUT_DIRECTORY
+echo "1.-------zipalign-------"
+$ANDROID_HOME/build-tools/29.0.2/zipalign -v -p 4 com.companyname.androidxa.apk my-app-unsigned-aligned.apk
+echo "2.-------remove original apk------"
+rm com.companyname.androidxa.apk
+echo "3.-------signing-------"
+$ANDROID_HOME/build-tools/29.0.2/apksigner sign --ks $APPCENTER_SOURCE_DIRECTORY/AndroidXA/my-release-key.jks --ks-pass pass:"123456" --out com.companyname.androidxa.apk my-app-unsigned-aligned.apk
+echo "4.-----remove cached file------"
+rm my-app-unsigned-aligned.apk
+echo "5.----completed, verify it----"
+$ANDROID_HOME/build-tools/29.0.2/apksigner verify --verbose --print-certs com.companyname.androidxa.apk
 
