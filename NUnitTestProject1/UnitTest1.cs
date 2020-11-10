957b67ade21794cca1d3dd842335ab92b98b8fac
Abraham Qian <qianyonglian@outlook.com>
2020-11-10 15:27:03 +0800

    nunit

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -12,7 +12,12 @@ namespace NUnitTestProject1
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Assert.Pass("Hello");
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass("ABD");
         }
     }
 }
