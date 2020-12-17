d3a3105eeb28703b723951a92286a397d96fe13b
Abraham Qian <qianyonglian@outlook.com>
2020-12-17 15:33:43 +0800

    add C#8 feature

--- AndroidXA/MainActivity.cs
+++ AndroidXA/MainActivity.cs
@@ -14,6 +14,17 @@ using System;
 
 namespace AndroidXA
 {
+    public enum Rainbow
+    {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet
+    }
+
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
@@ -67,6 +78,33 @@ namespace AndroidXA
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        }
+        public static RGBColor FromRainbow(Rainbow colorBand) =>
+    colorBand switch
+    {
+        Rainbow.Red => new RGBColor(0xFF, 0x00, 0x00),
+        Rainbow.Orange => new RGBColor(0xFF, 0x7F, 0x00),
+        Rainbow.Yellow => new RGBColor(0xFF, 0xFF, 0x00),
+        Rainbow.Green => new RGBColor(0x00, 0xFF, 0x00),
+        Rainbow.Blue => new RGBColor(0x00, 0x00, 0xFF),
+        Rainbow.Indigo => new RGBColor(0x4B, 0x00, 0x82),
+        Rainbow.Violet => new RGBColor(0x94, 0x00, 0xD3),
+        _ => throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand)),
+    };
+    }
+    public class RGBColor
+    {
+        public int A { get; set; } = 0;
+        public int B { get; set; } = 1;
+        public int C { get; set; } = 2;
+
+        public RGBColor(int a,int b,int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+
+            Console.WriteLine(a+b+c);
         }
     }
 }
