e49f1b69e0f2f574d0248d403fd23cbed8f9e1f5
Abraham-Qian <v-abqian@microsoft.com>
2021-01-04 15:47:05 +0800

    migrate to Azure DevOps

--- AndroidXA/appcenter-post-clone.sh
+++ AndroidXA/appcenter-post-clone.sh
@@ -1,3 +1,21 @@
+echo "-----Android SDK---"
+echo $ANDROID_HOME
+ls $ANDROID_HOME
+echo "------Android SDK----"
+ls $ANDROID_HOME/build-tools/
+echo "-----node----"
+node --version
+npm --version
+echo "----JDK-----"
+java -version
+echo "-----------"
+echo "-----msbuild----"
+where msbuild
+msbuild --version
+echo "------csc-------"
+where csc
+csc --version
+
 
 
 
